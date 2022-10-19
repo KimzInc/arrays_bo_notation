1e3989753585828a2d3765416311e4c3a6e00a39
KimzInc <kmtflx@gmail.com>
2022-10-19 20:01:19 +0300

    changes on index

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] intArray = new int[7];
+
+        intArray[0] = 200;
+
+        intArray[1] = 10;
+
+        intArray[2] = -1;
+
+        intArray[3] = 5;
+
+        intArray[4] = 6;
+
+        intArray[5] = 7;
+
+        intArray[6] = 9;
+
+       /* for (int i = 0; i < intArray.length; i++) {
+            System.out.println("["+i+"] "+ intArray[i]);
+        }*/
+
+       /*
+       retrieval with no known index position, is O(n) linear time complexity
+       retrieval with known index position, is O(1) constant time complexity
+       */
+
+        int index = 7;
+        for (int i = 0; i < intArray.length; i++) {
+            if (intArray[i] == 12) {
+                index = i;
+                break;
+            }
+        }
+        System.out.println("index is "+ index);
     }
 }
